a9c5b73300ce4bf585838dd9f7eb3cf7a342c8c6
NickoLu <hick64077@gmail.com>
2026-01-03 22:45:35 +0500

    fix: delete getUser func

--- app/_shared/components/ErrorTooltip.tsx
+++ app/_shared/components/ErrorTooltip.tsx
@@ -1,7 +1,7 @@
-export function ErrorTooltip() {
+export function ErrorTooltip({ error }: { error: any }) {
   return (
     <div className="pointer-events-none fixed inset-0 flex items-center justify-center">
-      <p className="pointer-events-auto text-center text-4xl text-gray-400">Network is not found</p>
+      <p className="pointer-events-auto text-center text-4xl text-gray-400">{error}</p>
     </div>
   );
 }
